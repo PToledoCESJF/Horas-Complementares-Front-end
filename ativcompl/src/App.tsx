c4910e680ec67d3db236322f5c2d973bcaa83fc6
Paulo Toledo <paulodat.902522195@uniacademia.edu.br>
2021-06-04 16:50:27 -0300

    Preparando o ambiente

--- ativcompl/src/App.tsx
+++ ativcompl/src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> Aqui a mágica acontece
         </p>
         <a
           className="App-link"
@@ -16,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aqui também
         </a>
       </header>
     </div>
